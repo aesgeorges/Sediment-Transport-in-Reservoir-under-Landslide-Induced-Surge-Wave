2878754a9740a9497af1e41ec3f029026a584e67
Alexandre Georges <erichaitian97@gmail.com>
2021-07-21 13:29:56 -0700

    semi implicit progress

--- 3D Semi-Implicit Solver/main.py
+++ 3D Semi-Implicit Solver/main.py
@@ -1,22 +1,25 @@
 import numpy as np
 from setup import *
 from advance import *
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-N = 10
-L = 20
-W = 20
-H = 20
+N = 40
+L = 5
+W = 5
+H = 5
 dx = L/N
 dy = W/N
 dz = H/N
-dt = 60
+dt = 30
 nu_h = 0.0001
 nu_v = 0.0001
 f = 0.0000891 # Coriolis Parameter - at San Francisco latitude 37.773972
 
 U = np.zeros((N,N,N))
 V = np.zeros((N,N,N))
-W = np.zeros((N,N,N))
+#W = np.zeros((N,N,N))
 Gu = np.zeros((N,N,N))
 Gv = np.zeros((N,N,N))
 deltaZ = dz*np.ones(N)
@@ -25,12 +28,29 @@ Cz = 50
 surf = np.zeros((N,N)) # Surface - eta in x,z
 length = np.arange(0,L,dx)
 for i in range(N):
-    surf[i,:] = length[i]*0.5 + 15
+    surf[i,:] = length[i]*0.5 + 5
 
-Fu, Fv = F_op(U, V, f, nu_h, dt, dx, dy, N)
+X = np.arange(0, W, dx)
+Y = np.arange(0, L, dy)
+X, Y = np.meshgrid(X, Y)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+Fu, Fv = F_op(U, V, f, nu_h, dt, dx, dy, dz, N)
 U,V,deltaZ,Gu,Gv,A = set_matrices(U,V,Cz,Fu,Fv,Gu,Gv,deltaZ,N,dt,dz,nu_v)
-surf, U, V = advance(surf,N,U,V,deltaZ,Gu,Gv,A,dt,dx,dy)
-print(surf)
+#surf, U, V = advance(surf,N,U,V,deltaZ,Gu,Gv,A,dt,dx,dy)
+#print(surf)
 #print(U)
 #print(V)
 
+total_time = 900
+
+def animate(i, surf, U, V, Gu, Gv):
+    ax.clear()
+    Gu,Gv = update_matrices(U,V,f,nu_h,Gu,Gv,deltaZ,N,dt,dx,dy,dz,nu_v)
+    surf, U, V = advance(surf,N,U,V,deltaZ,Gu,Gv,A,dt,dx,dy)
+    ax.plot_surface(X,Y,surf,rstride=1, cstride=1, cmap='cool', linewidth=0, antialiased=False)
+
+ani = animation.FuncAnimation(fig, animate, frames = total_time, fargs=(surf, U, V, Gu, Gv), interval = dt)
+ani.save('Visualization/semiimplicitN40.gif', writer='imagemagick', fps=24)
+#plt.show()
--- 3D Semi-Implicit Solver/setup.py
+++ 3D Semi-Implicit Solver/setup.py
@@ -3,13 +3,13 @@ import math
 from scipy.sparse import diags
 
 # Interpolator
-def interpolator(x, i, j, k):
-    u_ini = 0.4
-    v_ini = 0
+def interpolator(x,u,v, i, j, k, dt, dx, dy, dz):
+    u_ini = abs(u[i,j,k])
+    v_ini = abs(v[i,j,k])
     w_ini = 0
-    a = 0.5 #u_ini*(dt/dx)
-    b = 0.5 #v_ini*(dt/dy)
-    c = 0.5 #w_ini*(dt/dz)
+    a = min(u_ini*(dt/dx),1)
+    b = min(v_ini*(dt/dy),1)
+    c = 0.03#w_ini*(dt/dz)
     l = int(math.modf(a)[1])
     m = int(math.modf(b)[1])
     n = int(math.modf(c)[1])
@@ -20,27 +20,27 @@ def interpolator(x, i, j, k):
     return c_interp
 
 # Solve F operator
-def F_op(u, v, f, nu, dt, dx, dy, N):
+def F_op(u, v, f, nu, dt, dx, dy, dz, N):
     Fu = np.zeros((N, N, N))
     Fv = np.zeros((N, N, N))
     print("Computing F operator")
     for i in range(1, N-1):
         for j in range(1, N-1):
             for k in range(N):
-                u_interp = interpolator(u, i, j, k)
-                v_interp = interpolator(v, i, j, k)
+                u_interp = interpolator(u, u,v,i, j, k, dt, dx, dy, dz)
+                v_interp = interpolator(v, u,v,i, j, k, dt, dx, dy, dz)
 
-                u_interpP = interpolator(u, i+1, j, k)
-                u_interpM = interpolator(u, i-1, j, k)
-                u_interp_jP = interpolator(u, i, j+1, k)
-                u_interp_jM = interpolator(u, i, j-1, k)
-                Fu[i,j,k] = u_interp + nu*(dt/(dx**2))*(u_interpP - 2*u_interp + u_interpM) + (1/(dy**2))*(u_interp_jP - 2*u_interp + u_interp_jM) + f*dt*v_interp
+                u_interpP = interpolator(u, u,v,i+1, j, k, dt, dx, dy, dz)
+                u_interpM = interpolator(u, u,v,i-1, j, k, dt, dx, dy, dz)
+                u_interp_jP = interpolator(u, u,v,i, j+1, k, dt, dx, dy, dz)
+                u_interp_jM = interpolator(u, u,v,i, j-1, k, dt, dx, dy, dz)
+                Fu[i,j,k] = u_interp + (nu*dt)*((1/(dx**2))*(u_interpP - 2*u_interp + u_interpM) + (1/(dy**2))*(u_interp_jP - 2*u_interp + u_interp_jM)) + f*dt*v_interp
 
-                v_interpP = interpolator(v, i+1, j, k)
-                v_interpM = interpolator(v, i-1, j, k)
-                v_interp_jP = interpolator(v, i, j+1, k)
-                v_interp_jM = interpolator(v, i, j-1, k)
-                Fv[i,j,k] = v_interp + nu*(dt/(dy**2))*(v_interpP - 2*v_interp + v_interpM) + (1/(dy**2))*(v_interp_jP - 2*v_interp + v_interp_jM) + f*dt*u_interp
+                v_interpP = interpolator(v, u,v,i+1, j, k, dt, dx, dy, dz)
+                v_interpM = interpolator(v, u,v,i-1, j, k, dt, dx, dy, dz)
+                v_interp_jP = interpolator(v, u,v,i, j+1, k, dt, dx, dy, dz)
+                v_interp_jM = interpolator(v, u,v,i, j-1, k, dt, dx, dy, dz)
+                Fv[i,j,k] = v_interp + (nu*dt)*((1/(dy**2))*(v_interpP - 2*v_interp + v_interpM) + (1/(dy**2))*(v_interp_jP - 2*v_interp + v_interp_jM)) + f*dt*u_interp
 
     return Fu, Fv
 
@@ -55,8 +55,8 @@ def tridiag_maker(a,b,c,N):
 # Setting up all 3D arrays/matrices
 def set_matrices(U,V,Cz,Fu,Fv,Gu,Gv,deltaZ,N,dt,dz,nu_v):
     print('setting up matrices and 3D arrays...')
-    str_x = 5
-    str_y = 5
+    str_x = 0.05
+    str_y = 0.05
     bot_str = ((9.81*dt)/Cz**2)*(math.sqrt(U[0,0,-1]**2 + V[0,0,-1]**2))
     # Setting up G arrays
     for i in range(N):
